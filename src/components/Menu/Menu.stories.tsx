aa1087979841b5966fb1a93ecb0f372e32d4410e
jackice <275500122@qq.com>
2024-08-23 20:53:10 +0800

    feat: 中文登出

--- src/components/Menu/Menu.stories.tsx
+++ src/components/Menu/Menu.stories.tsx
@@ -28,191 +28,191 @@ import Button from "../Button/Button";
 import Box from "../Box/Box";
 
 export default {
-  title: "MDS/Layout/Menu",
-  component: Menu,
-  argTypes: {},
+    title: "MDS/Layout/Menu",
+    component: Menu,
+    argTypes: {},
 } as Meta<typeof Menu>;
 
 const Template: Story<MenuProps> = ({ options, horizontal, endComponent }) => {
-  const [menuCollapsed, setMenuCollapsed] = useState<boolean>(false);
+    const [menuCollapsed, setMenuCollapsed] = useState<boolean>(false);
 
-  return (
-    <StoryThemeProvider>
-      <GlobalStyles />
-      <Menu
-        isOpen={menuCollapsed}
-        displayGroupTitles
-        options={options}
-        applicationLogo={{ applicationName: "console", subVariant: "AGPL" }}
-        callPathAction={(path) => {
-          alert(`Called Path "${path}"`);
-        }}
-        signOutAction={() => {
-          alert("Sign Out!");
-        }}
-        collapseAction={() => {
-          setMenuCollapsed(!menuCollapsed);
-          console.log("COLLAPSE!");
-        }}
-        horizontal={horizontal}
-        currentPath={"/testPath1"}
-        endComponent={endComponent}
-      />
-    </StoryThemeProvider>
-  );
+    return (
+        <StoryThemeProvider>
+            <GlobalStyles />
+            <Menu
+                isOpen={menuCollapsed}
+                displayGroupTitles
+                options={options}
+                applicationLogo={{ applicationName: "console", subVariant: "AGPL" }}
+                callPathAction={(path) => {
+                    alert(`Called Path "${path}"`);
+                }}
+                signOutAction={() => {
+                    alert("登出!");
+                }}
+                collapseAction={() => {
+                    setMenuCollapsed(!menuCollapsed);
+                    console.log("COLLAPSE!");
+                }}
+                horizontal={horizontal}
+                currentPath={"/testPath1"}
+                endComponent={endComponent}
+            />
+        </StoryThemeProvider>
+    );
 };
 
 export const Default = Template.bind({});
 
 const options = [
-  {
-    icon: <TestIcon />,
-    path: "/testPath1",
-    name: "Test 1",
-    group: "Group 1",
-    id: "test1",
-    onClick: (path) => {
-      console.log("Custom Click Action", path);
-    },
-  },
-  {
-    icon: <TestIcon />,
-    name: "Test X",
-    group: "Group 1",
-    id: "testX",
-    onClick: (path) => {
-      console.log("Custom Click Action NO PATH", path);
-    },
-  },
-  {
-    icon: <TestIcon />,
-    path: "/testPath2",
-    name: "Test 2",
-    group: "Group 1",
-    id: "test2",
-    badge: true,
-  },
-  {
-    icon: <TestIcon />,
-    path: "/testPath3",
-    name: "Test 3",
-    group: "Group 1",
-    id: "tes3",
-  },
-  {
-    icon: <TestIcon />,
-    path: "/testPath4",
-    name: "Test 4",
-    group: "Group 2",
-    id: "tes4",
-  },
-  {
-    icon: <TestIcon />,
-    name: "Test 5",
-    group: "Group 2",
-    id: "test5",
-    children: [
-      {
+    {
         icon: <TestIcon />,
-        path: "/subPath1",
-        name: "Sublevel 1",
+        path: "/testPath1",
+        name: "Test 1",
+        group: "Group 1",
+        id: "test1",
+        onClick: (path) => {
+            console.log("Custom Click Action", path);
+        },
+    },
+    {
+        icon: <TestIcon />,
+        name: "Test X",
+        group: "Group 1",
+        id: "testX",
+        onClick: (path) => {
+            console.log("Custom Click Action NO PATH", path);
+        },
+    },
+    {
+        icon: <TestIcon />,
+        path: "/testPath2",
+        name: "Test 2",
+        group: "Group 1",
+        id: "test2",
+        badge: true,
+    },
+    {
+        icon: <TestIcon />,
+        path: "/testPath3",
+        name: "Test 3",
+        group: "Group 1",
+        id: "tes3",
+    },
+    {
+        icon: <TestIcon />,
+        path: "/testPath4",
+        name: "Test 4",
+        group: "Group 2",
+        id: "tes4",
+    },
+    {
+        icon: <TestIcon />,
+        name: "Test 5",
+        group: "Group 2",
+        id: "test5",
+        children: [
+            {
+                icon: <TestIcon />,
+                path: "/subPath1",
+                name: "Sublevel 1",
+                group: "Group 2",
+                id: "testl1",
+            },
+            {
+                icon: <TestIcon />,
+                path: "/subPath2",
+                name: "Sublevel 2",
+                group: "Group 2",
+                id: "testl2",
+            },
+            {
+                icon: <TestIcon />,
+                path: "/subPath3",
+                name: "Sublevel 3",
+                group: "Group 2",
+                id: "testl3",
+            },
+        ],
+    },
+    {
+        icon: <TestIcon />,
+        path: "https://min.io/",
+        name: "External URL",
         group: "Group 2",
         id: "testl1",
-      },
-      {
+    },
+    {
         icon: <TestIcon />,
-        path: "/subPath2",
-        name: "Sublevel 2",
+        name: "Hidden",
         group: "Group 2",
-        id: "testl2",
-      },
-      {
-        icon: <TestIcon />,
-        path: "/subPath3",
-        name: "Sublevel 3",
-        group: "Group 2",
-        id: "testl3",
-      },
-    ],
-  },
-  {
-    icon: <TestIcon />,
-    path: "https://min.io/",
-    name: "External URL",
-    group: "Group 2",
-    id: "testl1",
-  },
-  {
-    icon: <TestIcon />,
-    name: "Hidden",
-    group: "Group 2",
-    id: "test5",
-    children: [],
-  },
+        id: "test5",
+        children: [],
+    },
 ];
 
 Default.args = {
-  options,
+    options,
 };
 
 export const SubpathSelected = Template.bind({});
 
 SubpathSelected.args = {
-  options,
+    options,
 };
 
 export const Horizontal = Template.bind({});
 
 Horizontal.args = {
-  horizontal: true,
-  options: [
-    {
-      icon: <ChatIcon />,
-      path: "/testPath1",
-      name: "Test 1",
-      group: "Group 1",
-      id: "test1",
-      onClick: (path) => {
-        console.log("Custom Click Action", path);
-      },
-    },
-    {
-      icon: <LambdaNotificationsIcon />,
-      name: "Test 5",
-      group: "Group 2",
-      id: "test5",
-      children: [
+    horizontal: true,
+    options: [
         {
-          icon: <TestIcon />,
-          path: "/testPath1",
-          name: "Sublevel 1",
-          group: "Group 2",
-          id: "testl1",
+            icon: <ChatIcon />,
+            path: "/testPath1",
+            name: "Test 1",
+            group: "Group 1",
+            id: "test1",
+            onClick: (path) => {
+                console.log("Custom Click Action", path);
+            },
         },
         {
-          icon: <TestIcon />,
-          path: "/subPath2",
-          name: "Sublevel 2",
-          group: "Group 2",
-          id: "testl2",
+            icon: <LambdaNotificationsIcon />,
+            name: "Test 5",
+            group: "Group 2",
+            id: "test5",
+            children: [
+                {
+                    icon: <TestIcon />,
+                    path: "/testPath1",
+                    name: "Sublevel 1",
+                    group: "Group 2",
+                    id: "testl1",
+                },
+                {
+                    icon: <TestIcon />,
+                    path: "/subPath2",
+                    name: "Sublevel 2",
+                    group: "Group 2",
+                    id: "testl2",
+                },
+                {
+                    icon: <TestIcon />,
+                    path: "/subPath3",
+                    name: "Sublevel 3",
+                    group: "Group 2",
+                    id: "testl3",
+                },
+            ],
         },
-        {
-          icon: <TestIcon />,
-          path: "/subPath3",
-          name: "Sublevel 3",
-          group: "Group 2",
-          id: "testl3",
-        },
-      ],
-    },
-  ],
+    ],
 };
 
 export const MobileAutoDisabled = Template.bind({});
 
 MobileAutoDisabled.args = {
-  mobileModeAuto: false,
-  options,
+    mobileModeAuto: false,
+    options,
 };
 
 export const VerticalNoOptions = Template.bind({});
@@ -222,34 +222,34 @@ VerticalNoOptions.args = {};
 export const HorizontalNoOptions = Template.bind({});
 
 HorizontalNoOptions.args = {
-  horizontal: true,
+    horizontal: true,
 };
 
 export const EndComponent = Template.bind({});
 
 EndComponent.args = {
-  endComponent: (
-    <Box sx={{ display: "flex", justifyContent: "center" }}>
-      <Button
-        id={"randon-button"}
-        icon={<TestIcon />}
-        onClick={() => alert("Extra!")}
-      />
-    </Box>
-  ),
+    endComponent: (
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <Button
+                id={"randon-button"}
+                icon={<TestIcon />}
+                onClick={() => alert("Extra!")}
+            />
+        </Box>
+    ),
 };
 
 export const EndComponentHorizontal = Template.bind({});
 
 EndComponentHorizontal.args = {
-  horizontal: true,
-  endComponent: (
-    <Box sx={{ display: "flex", justifyContent: "center" }}>
-      <Button
-        id={"randon-button"}
-        icon={<TestIcon />}
-        onClick={() => alert("Extra!")}
-      />
-    </Box>
-  ),
+    horizontal: true,
+    endComponent: (
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <Button
+                id={"randon-button"}
+                icon={<TestIcon />}
+                onClick={() => alert("Extra!")}
+            />
+        </Box>
+    ),
 };
--- src/components/Menu/MobileMenu/MobileMenuList.tsx
+++ src/components/Menu/MobileMenu/MobileMenuList.tsx
@@ -29,151 +29,151 @@ import LogoutIcon from "../../Icons/LogoutIcon";
 import AlertCloseIcon from "../../Icons/AlertCloseIcon";
 
 const MobileMenuContainer = styled.div<MenuConstructProps>(({ theme, sx }) => {
-  return {
-    width: "100vw",
-    height: "100vh",
-    overflow: "auto",
-    position: "fixed",
-    top: 0,
-    left: 0,
-    background: get(
-      theme,
-      "menu.vertical.background",
-      lightColors.menuBackground,
-    ),
-    transitionDuration: "0.3s",
-    "& .menuContainer": {
-      height: "inherit",
-      position: "relative",
-      display: "flex",
-      flexDirection: "column",
-      "& .collapseButton": {
-        position: "absolute",
-        right: 15,
-        top: 15,
-        "& > svg": {
-          width: 20,
-          height: 20,
-          fill: get(
+    return {
+        width: "100vw",
+        height: "100vh",
+        overflow: "auto",
+        position: "fixed",
+        top: 0,
+        left: 0,
+        background: get(
             theme,
-            "menu.vertical.menuCollapseColor",
-            lightColors.menuCollapseColor,
-          ),
+            "menu.vertical.background",
+            lightColors.menuBackground,
+        ),
+        transitionDuration: "0.3s",
+        "& .menuContainer": {
+            height: "inherit",
+            position: "relative",
+            display: "flex",
+            flexDirection: "column",
+            "& .collapseButton": {
+                position: "absolute",
+                right: 15,
+                top: 15,
+                "& > svg": {
+                    width: 20,
+                    height: 20,
+                    fill: get(
+                        theme,
+                        "menu.vertical.menuCollapseColor",
+                        lightColors.menuCollapseColor,
+                    ),
+                },
+            },
         },
-      },
-    },
-    "& .menuLogoContainer": {
-      display: "flex",
-      justifyContent: "center",
-      position: "relative",
-      margin: "20px 30px 0",
-      "& svg": {
-        width: 150,
-      },
-    },
-    "& .collapsedMenuHeader": {
-      display: "none",
-    },
-    "& .menuItems": {
-      display: "flex",
-      flexDirection: "column",
-      flexGrow: 1,
-      height: "100%",
-    },
-    "& .menuHeaderContainer": {
-      cursor: "pointer",
-    },
-    ...sx,
-  };
+        "& .menuLogoContainer": {
+            display: "flex",
+            justifyContent: "center",
+            position: "relative",
+            margin: "20px 30px 0",
+            "& svg": {
+                width: 150,
+            },
+        },
+        "& .collapsedMenuHeader": {
+            display: "none",
+        },
+        "& .menuItems": {
+            display: "flex",
+            flexDirection: "column",
+            flexGrow: 1,
+            height: "100%",
+        },
+        "& .menuHeaderContainer": {
+            cursor: "pointer",
+        },
+        ...sx,
+    };
 });
 
 const MobileMenuList: FC<MenuProps> = ({
-  applicationLogo,
-  options,
-  displayGroupTitles,
-  signOutAction,
-  callPathAction,
-  collapseAction,
-  currentPath,
-  endComponent,
+    applicationLogo,
+    options,
+    displayGroupTitles,
+    signOutAction,
+    callPathAction,
+    collapseAction,
+    currentPath,
+    endComponent,
 }) => {
-  let currTitle = "";
+    let currTitle = "";
 
-  return (
-    <MobileMenuContainer>
-      <Box className={"menuContainer"}>
-        <Box className={"menuHeaderContainer"} onClick={collapseAction}>
-          <Box className={"collapseButton"}>
-            <AlertCloseIcon />
-          </Box>
-          <Box className={"menuLogoContainer"}>
-            <ApplicationLogo inverse {...applicationLogo} />
-          </Box>
-          <Box className={"collapsedMenuHeader"}>
-            <Tooltip tooltip={"Expand Menu"}>
-              <span className={"collapsedIcon"}>
-                <MinIOTierIconXs />
-              </span>
-            </Tooltip>
-          </Box>
-        </Box>
-        <Box className={"menuItems"}>
-          {options &&
-            options.map((option) => {
-              let extraItem = null;
+    return (
+        <MobileMenuContainer>
+            <Box className={"menuContainer"}>
+                <Box className={"menuHeaderContainer"} onClick={collapseAction}>
+                    <Box className={"collapseButton"}>
+                        <AlertCloseIcon />
+                    </Box>
+                    <Box className={"menuLogoContainer"}>
+                        <ApplicationLogo inverse {...applicationLogo} />
+                    </Box>
+                    <Box className={"collapsedMenuHeader"}>
+                        <Tooltip tooltip={"Expand Menu"}>
+                            <span className={"collapsedIcon"}>
+                                <MinIOTierIconXs />
+                            </span>
+                        </Tooltip>
+                    </Box>
+                </Box>
+                <Box className={"menuItems"}>
+                    {options &&
+                        options.map((option) => {
+                            let extraItem = null;
 
-              if (displayGroupTitles && option.group) {
-                if (currTitle !== option.group) {
-                  currTitle = option.group;
+                            if (displayGroupTitles && option.group) {
+                                if (currTitle !== option.group) {
+                                    currTitle = option.group;
 
-                  extraItem = <MenuSectionHeader label={option.group} />;
-                }
-              }
+                                    extraItem = <MenuSectionHeader label={option.group} />;
+                                }
+                            }
 
-              const onClickAction = (path: string) => {
-                if (option.onClick) {
-                  option.onClick(path);
-                  collapseAction();
-                  return;
-                }
+                            const onClickAction = (path: string) => {
+                                if (option.onClick) {
+                                    option.onClick(path);
+                                    collapseAction();
+                                    return;
+                                }
 
-                callPathAction(path);
-                collapseAction();
-              };
+                                callPathAction(path);
+                                collapseAction();
+                            };
 
-              return (
-                <Fragment key={`menu-section-${option.group}-${option.id}`}>
-                  {extraItem}
-                  <MenuItem
-                    onClick={onClickAction}
-                    icon={option.icon}
-                    name={option.name}
-                    group={option.group}
-                    id={option.id}
-                    path={option.path}
-                    currentPath={currentPath}
-                    badge={option.badge}
-                    children={option.children}
-                  />
-                </Fragment>
-              );
-            })}
-          {signOutAction && (
-            <Box sx={{ marginTop: "auto" }}>
-              {endComponent}
-              <MenuSectionHeader label={""} />
-              <MenuItem
-                group={"common"}
-                name={"Sign Out"}
-                icon={<LogoutIcon />}
-                onClick={signOutAction}
-              />
+                            return (
+                                <Fragment key={`menu-section-${option.group}-${option.id}`}>
+                                    {extraItem}
+                                    <MenuItem
+                                        onClick={onClickAction}
+                                        icon={option.icon}
+                                        name={option.name}
+                                        group={option.group}
+                                        id={option.id}
+                                        path={option.path}
+                                        currentPath={currentPath}
+                                        badge={option.badge}
+                                        children={option.children}
+                                    />
+                                </Fragment>
+                            );
+                        })}
+                    {signOutAction && (
+                        <Box sx={{ marginTop: "auto" }}>
+                            {endComponent}
+                            <MenuSectionHeader label={""} />
+                            <MenuItem
+                                group={"common"}
+                                name={"登出"}
+                                icon={<LogoutIcon />}
+                                onClick={signOutAction}
+                            />
+                        </Box>
+                    )}
+                </Box>
             </Box>
-          )}
-        </Box>
-      </Box>
-    </MobileMenuContainer>
-  );
+        </MobileMenuContainer>
+    );
 };
 
 export default MobileMenuList;
