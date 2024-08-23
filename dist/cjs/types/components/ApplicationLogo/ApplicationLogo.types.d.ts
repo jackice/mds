a93e009dbd3661108524493a5886818ab1471630
jackice <275500122@qq.com>
2024-08-23 18:52:22 +0800

    first commit

---
+++ dist/cjs/types/components/ApplicationLogo/ApplicationLogo.types.d.ts
@@ -0,0 +1,33 @@
+import React from "react";
+export interface ApplicationLogoProps {
+  applicationName:
+    | "console"
+    | "operator"
+    | "directpv"
+    | "kes"
+    | "subnet"
+    | "subnetops"
+    | "cloud"
+    | "releases"
+    | "vmbroker"
+    | "eureka"
+    | "kms"
+    | "loadbalancer"
+    | "index"
+    | "cache"
+    | "monitor"
+    | "observe"
+    | "missioncontrol"
+    | "globalconsole"
+    | "minio"
+    | "enterprise";
+  subVariant?:
+    | "simple"
+    | "AGPL"
+    | "standard"
+    | "enterprise"
+    | "new"
+    | "enterpriseos";
+  inverse?: boolean;
+  onClick?: React.MouseEventHandler<any> | undefined;
+}
